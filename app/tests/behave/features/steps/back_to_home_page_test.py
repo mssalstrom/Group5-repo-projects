6a666a89fb2f2c4362935eba37f857a7f223d823
DmytHol <dmytroholovniapro@gmail.com>
2023-10-31 00:23:41 -0400

    back_to_home_page.feature and tests was added

--- app/tests/behave/features/steps/back_to_home_page_test.py
+++ app/tests/behave/features/steps/back_to_home_page_test.py
@@ -1,41 +1,53 @@
 from behave import *
-import selenium
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-"""RTM REFERENCE: URL Shortener TC7, TC8"""
 
-@given('I launch chrome browser')
+@given('I launch Firefox browser')
 def launch_browser(context):
-    context.driver = webdriver.Chrome()
+    context.driver = webdriver.Firefox()
 
-@when('open URL Shortener Home page')
-def open_home_page(context):
+
+@when('I open URL Shortener homepage')
+def open_homepage(context):
     context.driver.get("http://localhost:5000/")
-    context.driver.implicitly_wait(10)
 
-@when ('I enter the "{url}" url')
+
+@when('verify that the title is "127.0.0.1:5000"')
+def verify_title(context):
+    assert context.driver.title == "127.0.0.1:5000"
+
+
+@when('I enter the {url} url')
 def enter_url(context, url):
-    context.driver.find_element(By.NAME, 'url').send_keys(url)
+    context.driver.find_element(By.NAME, "url").send_keys(url)
 
-@when ('I enter shorten name "{short_name}"')
-def enter_short_name(context, short_name):
-    context.driver.find_element(By.NAME, 'code').send_keys(short_name)
 
-@when ('click on the shorten url button')
-def press_button(context):
-    context.driver.find_element(By.ID, 'shortenSubmit').click()
+@when('enter shorten name "{sn}"')
+def enter_short_name(context, sn):
+    context.driver.find_element(By.NAME, "code").send_keys(sn)
 
-@when ('I should be redirected to the /your_url page')
-def step_impl(context):
+
+@when('click on Shorten URL button')
+def press_button_shorten_url(context):
+    shorten_button = context.driver.find_element(By.ID, "shortenSubmit")
+    # Click the button
+    shorten_button.click()
+
+
+@when('user can see new page with short link')
+def verify_new_page(context):
+    # check for elements, text, or any other criteria on this page.
     assert context.driver.title == "Your URL"
 
-@when ('click on the back to home page button')
-def press_button(context):
-    context.driver.find_element(By.ID, 'backToHome').click()
 
-@then ('user must be successfully redirected to the home page')
-def step_impl(context):
-    assert context.driver.title == "URL Shortener"
+@when('I click on the link with id "return"')
+def press_return_button(context):
+    back_link = context.driver.find_element(By.ID, "return")
+    back_link.click()
+
+
+@then('successfully return to the home page')
+def verify_home_page(context):
+    assert context.driver.title == "URL Shortener", f"Expected title to be: {context.driver.title}"
     context.driver.close()
