df0322241c778cd35b6b9eaebd1fe7ebad0cc5db
Dylan Arone <daarone@my.waketech.edu>
2023-11-30 13:22:30 -0500

    Update Docstrings
    Updated docstrings for tests

--- app/tests/api_tests/test_get_requests.py
+++ app/tests/api_tests/test_get_requests.py
@@ -6,49 +6,91 @@ import requests_mock
 
 
 def test_get_posts():
-    """Tests GET request on /posts endpoint"""
+    """
+    Tests GET request on /posts endpoint
+    Args:
+        none
+    Returns:
+        none
+    """
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
     assert response.status_code == 200
     #print(response.json())
 
 
 def test_get_comments():
-    """Tests GET request on /comments endpoint"""
+    """
+    Tests GET request on /comments endpoint
+    Args:
+        none
+    Returns:
+        none
+    """
     response = requests.get("https://jsonplaceholder.typicode.com/comments")
     assert response.status_code == 200
     #print(response.json())
 
 
 def test_get_albums():
-    """Tests GET request on /albums endpoint"""
+    """
+    Tests GET request on /albums endpoint
+    Args:
+        none
+    Returns:
+        none
+    """
     response = requests.get("https://jsonplaceholder.typicode.com/albums")
     assert response.status_code == 200
     #print(response.json())
 
 
 def test_get_photos():
-    """Tests GET request on /photos endpoint"""
+    """
+    Tests GET request on /photos endpoint
+    Args:
+        none
+    Returns:
+        none
+    """
     response = requests.get("https://jsonplaceholder.typicode.com/photos")
     assert response.status_code == 200
     #print(response.json())
 
 
 def test_get_todos():
-    """Tests GET request on /todos endpoint"""
+    """
+    Tests GET request on /todos endpoint
+    Args:
+        none
+    Returns:
+        none
+    """
     response = requests.get("https://jsonplaceholder.typicode.com/todos")
     assert response.status_code == 200
     #print(response.json())
 
 
 def test_get_users():
-    """Tests GET request on /users endpoint"""
+    """
+    Tests GET request on /users endpoint
+    Args:
+        none
+    Returns:
+        none
+    """
     response = requests.get("https://jsonplaceholder.typicode.com/users")
     assert response.status_code == 200
     #print(response.json())
 
 
 def test_get_post_by_post_id():
-    """Tests GET request on /posts using specific post ID"""
+    """
+    Tests GET request on /posts using specific post ID
+    Args:
+        none
+    Returns:
+        none
+    """
     response = requests.get("https://jsonplaceholder.typicode.com/posts/1")     # Post ID = 1
     assert response.status_code == 200
     print()
@@ -56,7 +98,13 @@ def test_get_post_by_post_id():
 
 
 def test_get_comments_by_post_id():
-    """Tests GET request on /comments using specific post ID"""
+    """
+    Tests GET request on /comments using specific post ID
+    Args:
+        none
+    Returns:
+        none
+    """
     response = requests.get("https://jsonplaceholder.typicode.com/posts/1/comments")
     assert response.status_code == 200
     print()
@@ -64,7 +112,13 @@ def test_get_comments_by_post_id():
 
 
 def test_get_comments_by_post_id_using_query_string():
-    """Tests GET request on /comments using specific post ID"""
+    """
+    Tests GET request on /comments using specific post ID
+    Args:
+        none
+    Returns:
+        none
+    """
     response = requests.get("https://jsonplaceholder.typicode.com/comments?postId=1")
     assert response.status_code == 200
     print()
--- app/tests/behave/features/steps/back_to_home_page_test.py
+++ app/tests/behave/features/steps/back_to_home_page_test.py
@@ -5,31 +5,87 @@ from selenium.webdriver.common.by import By
 
 @given('I launch Firefox browser')
 def launch_browser(context):
+    """
+    Initializes an instance of selenium webdriver
+    Args:
+        context: context variable
+
+    Returns:
+        none
+    """
     context.driver = webdriver.Firefox()
 
 
 @when('I open URL Shortener homepage')
 def open_homepage(context):
+    """
+    Navigates to URL of web application
+    Args:
+        context: context variable
+            Has an initialized instance of selenium driver as an attribute
+
+    Returns:
+        none
+    """
     context.driver.get("http://localhost:5000/")
 
 
 @when('verify that the title is "127.0.0.1:5000"')
 def verify_title(context):
+    """
+    Ensure that the title matches the intended title
+    Args:
+        context:  context variable
+            Has an initialized instance of selenium driver as an attribute
+
+    Returns:
+        none
+
+    """
     assert context.driver.title == "127.0.0.1:5000"
 
 
 @when('I enter the {url} url')
 def enter_url(context, url):
+    """
+    Enters a URL into the appropriate text field
+    Args:
+        context: context variable
+            Has an initialized instance of selenium driver as an attribute
+        url: URL to be entered
+
+    Returns:
+        none
+    """
     context.driver.find_element(By.NAME, "url").send_keys(url)
 
 
 @when('enter shorten name "{sn}"')
 def enter_short_name(context, sn):
+    """
+    Enter a shortened name into the appropriate text field
+    Args:
+        context: context variable
+            Has an initialized instance of selenium driver as an attribute
+        sn: Shortened name to be entered
+
+    Returns:
+        none
+    """
     context.driver.find_element(By.NAME, "code").send_keys(sn)
 
 
 @when('click on Shorten URL button')
 def press_button_shorten_url(context):
+    """
+    Clicks on the form submit button
+    Args:
+        context: context variable
+            Has an initialized instance of selenium driver as an attribute
+
+    Returns:
+        none
+    """
     shorten_button = context.driver.find_element(By.ID, "shortenSubmit")
     # Click the button
     shorten_button.click()
@@ -37,17 +93,44 @@ def press_button_shorten_url(context):
 
 @when('user can see new page with short link')
 def verify_new_page(context):
+    """
+    Ensures that the program has navigated to the correct page
+    Args:
+        context: context variable
+            Has an initialized instance of selenium driver as an attribute
+
+    Returns:
+        none
+    """
     # check for elements, text, or any other criteria on this page.
     assert context.driver.title == "Your URL"
 
 
 @when('I click on the link with id "return"')
 def press_return_link(context):
+    """
+    Returns to the previous page
+    Args:
+        context: context variable
+            Has an initialized instance of selenium driver as an attribute
+
+    Returns:
+        none
+    """
     back_link = context.driver.find_element(By.ID, "return")
     back_link.click()
 
 
 @then('successfully return to the home page')
 def verify_home_page(context):
+    """
+    Ensures that the program has returned to the home page
+    Args:
+        context: context variable
+            Has an initialized instance of selenium driver as an attribute
+
+    Returns:
+        none
+    """
     assert context.driver.title == "URL Shortener", f"Expected title to be: {context.driver.title}"
     context.driver.close()
